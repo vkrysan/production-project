c6aed3717beab8e5ce0b9f302e93f169658795b9
vkrysan <krisanvladimir@yandex.ru>
2024-10-25 16:25:13 +0300

    fix loginSlice test

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -8,7 +8,7 @@ describe('loginSlice.test', () => {
         expect(loginReducer(
             state as LoginSchema,
             loginActions.setUsername('123123'),
-        )).toBe({ username: '123123' });
+        )).toEqual({ username: '123123' });
     });
 
     test('test set password', () => {
@@ -16,6 +16,6 @@ describe('loginSlice.test', () => {
         expect(loginReducer(
             state as LoginSchema,
             loginActions.setPassword('123123'),
-        )).toBe({ password: '123123' });
+        )).toEqual({ password: '123123' });
     });
 });
